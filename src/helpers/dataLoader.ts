f36b6366bbe99c884eb2468c393ffda4442fa13a
hasan  khaddour <hasan.b.khaddour@gmail.com>
2025-02-07 10:55:55 +0300

    Add Helpers Functions

---
+++ src/helpers/dataLoader.ts
@@ -0,0 +1,21 @@
+import * as d3 from 'd3' ;
+
+// function to load the data sert with latency
+export function DataLoader(dataSetUrl : string , latency : number, callback : any,...args: any ){
+    // 
+    setTimeout(()=>
+    
+      d3.csv(dataSetUrl)
+        .then((data ) => {
+          callback(data,...args);
+        })
+        .catch((err) => {
+          console.error("We are sorry, but an error occured ): !!.");
+          console.error(err);
+        }
+      ),
+      latency
+    );
+      
+  }
+  
